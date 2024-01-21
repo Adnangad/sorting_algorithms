1af994675f026753cd807a57d9d61a1c16be4637
Adnangad <gardobuyaadnan@gmail.com>
2024-01-21 05:56:40 -0800

    1-insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - sorts a doubly linked listed thru insertion sort
+ * @list:the list to be sorted
+ *
+ * Return:none.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *a;
+	listint_t *b;
+	listint_t *c, *temp;
+
+	if (*list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	a = (*list)->next;
+
+	while (a)
+	{
+		b = a->prev;
+		c = a;
+
+		while (b && b->n > c->n)
+		{
+			temp = c->next;
+
+			if (temp)
+				temp->prev = b;
+			c->prev = b->prev;
+
+			if (c->prev)
+				c->prev->next = c;
+			else
+				*list = c;
+			b->prev = c;
+			b->next = temp;
+			c->next = b;
+			print_list(*list);
+			b = c->prev;
+		}
+		a = a->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -7,6 +7,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+#include <stddef.h>
 typedef struct listint_s
 {
     const int n;
@@ -19,4 +20,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
 #endif
