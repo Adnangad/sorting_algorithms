2e2b6a2b1d6a472fd871081cfd655bcd6f43f45f
Adnangad <gardobuyaadnan@gmail.com>
2024-01-21 06:59:49 -0800

    2-selection

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,7 +14,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *b;
 	listint_t *c, *temp;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
