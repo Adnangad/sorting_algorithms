eea72318bd29f02c1aa1874c6f545afe6e31f9f5
Adnangad <gardobuyaadnan@gmail.com>
2024-01-23 08:02:48 -0800

    3-quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,75 +3,82 @@
 #include "sort.h"
 
 /**
- * part - Partition the array for quicksort
- * @arr: Array to be partitioned
- * @low: Starting index of the partition
- * @high: Ending index of the partition
- *
- * Return: Index of the pivot element after partition
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  */
-int part(int *arr, size_t low, size_t high)
+void swap(int *a, int *b)
 {
-	int start, end, temp, temp1, pivot;
-
-	start = high;
-	end = low - 1;
-	pivot = arr[high];
-
-	while (start > end)
-	{
-		while (arr[start] >= pivot)
-			start--;
-
-		while (arr[end] < pivot)
-			end++;
-
-		if (start > end)
-		{
-			temp = arr[start];
-			arr[start] = arr[end];
-			arr[end] = temp;
-			print_array(arr, high + 1);
-		}
-	}
-
-	temp1 = arr[high];
-	arr[high] = arr[end];
-	arr[end] = temp1;
-	print_array(arr, high + 1);
-
-	return (end);
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * helper - Recursive function to perform quicksort
- * @arr: Array to be sorted
- * @low: Starting index of the partition to be sorted
- * @high: Ending index of the partition to be sorted
+ * part - Lomuto partition scheme for quicksort.
+ * @array: Array to be partitioned.
+ * @low: Starting index of the array.
+ * @high: Ending index of the array.
+ * @size:siaze of array/
+ * Return: The partition index.
  */
-void helper(int *arr, size_t low, size_t high)
+int part(int *array, size_t size, int low, int high)
 {
-	int pi;
+	int *pivot = array + high;
+	int start, end;
+
+	for (start = end = low; end < high; end++)
+	{
+		if (array[end] < *pivot)
+		{
+			if (start < end)
+			{
+				swap(array + end, array + start);
+				print_array(array, size);
+			}
+			start++;
+		}
+	}
+	if (array[start] > *pivot)
+	{
+		swap(array + start, pivot);
+		print_array(array, size);
+	}
+	return (start);
+}
+
+/**
+ * helper - Recursive helper function for quicksort.
+ * @array: Array to be sorted.
+ * @low: Starting index of the array.
+ * @high: Ending index of the array.
+ * @size:size of array
+ *
+ * Return:none
+ */
+void helper(int *array, size_t size, int low, int high)
+{
+	int p;
 
 	if (low < high)
 	{
-		pi = part(arr, low, high);
-
-		if (pi > 0)
-			helper(arr, low, pi - 1);
-
-		if ((size_t)(pi + 1) <= high)
-			helper(arr, pi + 1, high);
+		p = part(array, size, low, high);
+		helper(array, size, low, p - 1);
+		helper(array, size, p + 1, high);
 	}
 }
 
 /**
- * quick_sort - Function to sort an array using quicksort algorithm
- * @array: Array to be sorted
- * @size: Size of the array
+ * quick_sort - Sorts an array using the quicksort algorithm.
+ * @array: Array to be sorted.
+ * @size: Number of elements in the array.
  */
 void quick_sort(int *array, size_t size)
 {
-	helper(array, 0, size - 1);
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	helper(array, size, 0, size - 1);
 }
 
